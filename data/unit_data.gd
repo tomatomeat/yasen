9985c83d2fd7232b7eddce0e59dc1d21c73db291
tomatomeat <tomatomeat0@gmail.com>
2026-02-18 13:20:18 +0900

    h

--- data/unit_data.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name UnitData
-
-@export var id: String = ""
-@export var display_name: String = ""
-@export var description: String = ""
-# 将来的な追加パラメータ（移動力や特殊能力）
-@export var move_range: int = 1
-@export var vision_range: int = 1
